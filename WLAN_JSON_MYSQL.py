357512758bda83ba3ea92d195453e4e1618ab095
Yuri Mecca <yuri.mecca@global.ntt>
2020-03-16 09:54:47 -0700

    Added python try function in WLAN_JSON

--- WLAN_JSON_MYSQL.py
+++ WLAN_JSON_MYSQL.py
@@ -1,40 +1,47 @@
-import pymysql, os, json
-
-file = "/home/devnet/wlan_sensor/server/DATABASE/DATABASE.json"
-json_data=open(file).read()
-json_obj = json.loads(json_data)
-
+import pymysql, os, json, time
 
 def WLAN_VALIDATE(val):
-   if val != None:
-        if type(val) is int:
-            #for x in val:
+       if val != None:
+            if type(val) is int:
+                #for x in val:
             #   print(x)
-            return str(val).encode('utf-8')
-        else:
-            return val
+                return str(val).encode('utf-8')
+            else:
+                return val
 
 
 def WLAN_MYSQL():
-   con = pymysql.connect(host = 'localhost',user = 'grafana',passwd = 'grafana',db = 'WLAN_SENSOR')
-   cursor = con.cursor()
+    try:
+       file = "/home/devnet/wlan_sensor/server/DATABASE/DATABASE.json"
+       json_data=open(file).read()
+       WLAN_JSON_OBJ = json.loads(json_data)
+       con = pymysql.connect(host = 'localhost',user = 'grafana',passwd = 'grafana',db = 'WLAN_SENSOR')
+       cursor = con.cursor()
 
-# parse json data to SQL insert
-   for i, item in enumerate(json_obj):
-       ID = WLAN_VALIDATE(item.get("WLAN_ID", None))
-       TIME = WLAN_VALIDATE(item.get("WLAN_TIME", None))
-       MAC = WLAN_VALIDATE(item.get("WLAN_HARDWARE_MAC", None))	
-       IPV4 = WLAN_VALIDATE(item.get("WLAN_IPV4", None))	
-       DATARATE= WLAN_VALIDATE(item.get("WLAN_RATE(Mbps)", None))	
-       RX_SIGNAL = WLAN_VALIDATE(item.get("WLAN_SIGNAL(dBm)", None))
-       TX_POWER = WLAN_VALIDATE(item.get("WLAN_TX_POWER(dBm)", None))	
-       RETRIES = WLAN_VALIDATE(item.get("WLAN_RETRIES", None))	
+       # parse json data to SQL insert
 
-       cursor.execute("INSERT INTO SENSOR_CLIENT (ID, TIME, MAC, IPV4, DATARATE, RX_SIGNAL, TX_POWER, RETRIES) VALUES (%s, %s, %s, %s, %s, %s, %s, %s )", (ID, TIME, MAC, IPV4, DATARATE, RX_SIGNAL, TX_POWER, RETRIES))
+       for i, item in enumerate(WLAN_JSON_OBJ):
+           ID = WLAN_VALIDATE(item.get("WLAN_ID", None))
+           TIME = WLAN_VALIDATE(item.get("WLAN_TIME", None))
+           MAC = WLAN_VALIDATE(item.get("WLAN_HARDWARE_MAC", None))	
+           IPV4 = WLAN_VALIDATE(item.get("WLAN_IPV4", None))	
+           DATARATE= WLAN_VALIDATE(item.get("WLAN_RATE(Mbps)", None))	
+           RX_SIGNAL = WLAN_VALIDATE(item.get("WLAN_SIGNAL(dBm)", None))
+           TX_POWER = WLAN_VALIDATE(item.get("WLAN_TX_POWER(dBm)", None))	
+           RETRIES = WLAN_VALIDATE(item.get("WLAN_RETRIES", None))	
 
-   con.commit()
-   con.close()
+           cursor.execute("INSERT INTO SENSOR_CLIENT (ID, TIME, MAC, IPV4, DATARATE, RX_SIGNAL, TX_POWER, RETRIES) VALUES (%s, %s, %s, %s, %s, %s, %s, %s )", (ID, TIME, MAC, IPV4, DATARATE, RX_SIGNAL, TX_POWER, RETRIES))
+
+       con.commit()
+       con.close()
+       print('Success!')
+
+    except:
+        print('Error')
+        time.sleep(5)
+        WLAN_MYSQL()
+
 
 if __name__ == '__main__':
-	WLAN_MYSQL()
+    WLAN_MYSQL()
 
