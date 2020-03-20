d8729621e2555d776a8b7b7460ddae94a9e8f81a
Yuri Mecca <yuri.mecca@global.ntt>
2020-03-20 09:03:36 -0400

    Adjusted MAIN to work with Active and Passive collection

--- MAIN.py
+++ MAIN.py
@@ -1,18 +1,56 @@
 import os
 import subprocess as sp
 import time
-import WLAN_JSON_MYSQL
+import WLAN_JSON_ACTIVE_MYSQL
+import WLAN_JSON_PASSIVE_MYSQL
 
-def WLAN_MOVE_FILE():
+def WLAN_PASSIVE_MOVE_FILE():
     try:
-        print("\n\n###################################################\n\nO) MOVING JSON FILE TO DATABASE FOLDER")
-        sp.getoutput('mv -f /home/devnet/wlan_sensor/client/DATABASE.json /home/devnet/wlan_sensor/server/DATABASE/DATABASE.json')
+        print("\n\n###################################################\n\nO) MOVING JSON FILE FROM PASSIVE COLLECT TO THE DATABASE FOLDER")
+        sp.getoutput('mv -f /home/devnet/wlan_sensor/client/PASSIVE_DATABASE.json /home/devnet/wlan_sensor/server/DATABASE/PASSIVE_DATABASE.json')
 
     except:
         time.sleep(1)
         print('----FAILED TO MOVE JSON FILE SKIPPING')
 	  
-def WLAN_DELETE_FILE():
+def WLAN_ACTIVE_MOVE_FILE():
+    try:
+        print("O) MOVING JSON FILE FROM ACTIVE COLLECT TO THE DATABASE FOLDER")
+        sp.getoutput('mv -f /home/devnet/wlan_sensor/client/ACTIVE_DATABASE.json /home/devnet/wlan_sensor/server/DATABASE/ACTIVE_DATABASE.json')
+
+    except:
+        time.sleep(1)
+        print('----FAILED TO MOVE JSON FILE SKIPPING')	  
+		
+def WLAN_IMPORT_PASSIVE_FILE():		
+
+    
+    print("O) TRYING TO IMPORT NEW JSON PASSIVE FILE TO THE DATABASE")
+
+    try:
+       WLAN_JSON_PASSIVE_MYSQL.WLAN_MYSQL()
+       print('----SUCCESS TO IMPORT')
+       WLAN_DELETE_PASSIVE_FILE()
+
+    except:
+       print('----FAILED TO IMPORT. SKIPPING')
+       time.sleep(1)
+	   
+def WLAN_IMPORT_ACTIVE_FILE():		
+
+    
+    print("O) TRYING TO IMPORT NEW JSON ACTIVE FILE TO THE DATABASE")
+
+    try:
+       WLAN_WLAN_JSON_ACTIVE_MYSQL.WLAN_MYSQL()
+       print('----SUCCESS TO IMPORT')
+       WLAN_DELETE_ACTIVE_FILE()
+
+    except:
+       print('----FAILED TO IMPORT. SKIPPING')
+       time.sleep(1)	   
+
+def WLAN_DELETE_PASSIVE_FILE():
     try:
         print("O) DELETING JSON FILE")
         sp.getoutput('rm -rf /home/devnet/wlan_sensor/server/DATABASE/DATABASE.json')
@@ -21,28 +59,15 @@ def WLAN_DELETE_FILE():
         time.sleep(1)
         print('----FAILED TO DELETE JSON FILE, SKIPPING') 
 		
-		
-def WLAN_IMPORT_FILE():		
-
-    
-    print("O) TRYING TO IMPORT NEW JSON FILE TO THE DATABASE")
-
-    try:
-       WLAN_JSON_MYSQL.WLAN_MYSQL()
-       print('----SUCCESS TO IMPORT')
-       WLAN_DELETE_FILE()
-
-    except:
-       print('----FAILED TO IMPORT. SKIPPING')
-       time.sleep(1)
-
 
 if __name__ == '__main__':
 
   while True:
-
-    WLAN_MOVE_FILE()
-    WLAN_IMPORT_FILE()
+    WLAN_PASSIVE_MOVE_FILE()
+    WLAN_ACTIVE_MOVE_FILE()
+    WLAN_IMPORT_PASSIVE_FILE()
+    WLAN_IMPORT_ACTIVE_FILE()
     print("O) COUNTING DOWN 10 SECONDS\n\n###################################################")
     time.sleep(10)
+
 
