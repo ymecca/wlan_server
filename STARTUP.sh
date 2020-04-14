4301ca900d6fff767e00d71178edaa7457e5c89e
Yuri Mecca <yuri.mecca@global.ntt>
2020-04-14 11:44:06 -0400

    adjusted startup files

--- STARTUP.sh
+++ STARTUP.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+killall python
+killall iperf3
 source /home/devnet/environments/wlan_sensor/bin/activate
 nohup python -u /home/devnet/wlan_sensor/server/MAIN.py > /home/devnet/wlan_sensor/server/LOG/LOGFILE.log &
-nohup iperf3 -s &
+nohup iperf3 -s -D &
 
